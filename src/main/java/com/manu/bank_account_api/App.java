24f7352b9eea6c78e1edd2093636cbef427d883d
manu1907 <euggio1907@gmail.com>
2022-01-19 16:20:56 +0100

    First Commit.

---
+++ src/main/java/com/manu/bank_account_api/App.java
@@ -0,0 +1,12 @@
+package com.manu.bank_account_api;
+
+import org.apache.log4j.Logger;
+
+public class App {
+
+    private static final Logger logger = Logger.getLogger(App.class);
+
+    public static void main( String[] args ) {
+        logger.info("The Bank Account API Project has been created today.");
+    }
+}
