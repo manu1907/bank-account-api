4cbcc9f29f9e8b07f574ed7f0425b94ab842bdad
manu1907 <euggio1907@gmail.com>
2022-01-20 13:19:44 +0100

    Changed JUnit version 4 to 5.

---
+++ src/test/java/com/manu/bank_account_api/AppTest.java
@@ -0,0 +1,16 @@
+package com.manu.bank_account_api;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
